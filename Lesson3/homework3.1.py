d8e9f7a5ed2824eb2b78688d47dd2bb263a87a51
Marharyta <mmaarrggoo01@gmail.com>
2025-04-07 11:01:50 +0200

    homeworknew3.1 3.2

--- Lesson3/homework3.1.py
+++ Lesson3/homework3.1.py
@@ -1,18 +1,17 @@
 number1 = float(input("Введіть число: "))
 operation = input("Введіть знак (-, +, /, *): ")
 number2 = float(input("Введіть число: "))
-if (operation == "/"
-        and number2 == 0):
-    print("Не можна на 0")
-else:
-    if operation == "+":
-        result = number1 + number2
-    elif operation == "-":
-        result = number1 - number2
-    elif operation == "*":
-        result = number1 * number2
-    elif operation == "/":
-        result = number1 / number2
+if operation == "+":
+    result = number1 + number2
+elif operation == "-":
+    result = number1 - number2
+elif operation == "*":
+    result = number1 * number2
+elif operation == "/":
+    if number2 == 0:
+        print(" не ділити на 0 ")
     else:
-        result = "Невірно операція!"
-    print(result)
+        result = number1 / number2
+else:
+    result = "Невірна операція!"
+print()
--- Lesson3/homework3.2.py
+++ Lesson3/homework3.2.py
@@ -1,12 +1,9 @@
-lst = [1,2,3, "4", [1,2,"3"]]
+lst = []
 if len(lst) >= 2:
     a = lst.pop()
     lst.insert(0, a)
-    print(lst)
-elif len(lst) == 1:
-        print([lst[0]])
-elif lst == ([]):
-    print (lst)
+elif len(lst) == 1 or lst == []:
+    print()
 else:
     print("False")
-
+print(lst)
