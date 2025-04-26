f89a29f620d0f2fce392cd81ee83ce69b1ceb3b3
Marharyta <mmaarrggoo01@gmail.com>
2025-04-26 09:18:51 +0200

    5.1 new

--- lesson5/homework5.1.py
+++ lesson5/homework5.1.py
@@ -1,16 +1,52 @@
 
+# import string
+# import keyword
+# name = input("Введіть потенційне ім’я змінної: ")
+# if name[0].isdigit():
+#     print(" false")
+# elif any(char.isupper() for char in name):
+#     print("false.")
+# elif any(char in string.punctuation.replace("_", "") or char == " " for char in name):
+#     print(" false.")
+# elif name.count("_") > 1:
+#     print( "false")
+# elif name in keyword.kwlist:
+#     print("false")
+# else:
+#     print("true")
+
 import string
 import keyword
-name = input("Введіть потенційне ім’я змінної: ")
+
+name = input("Введіть ім'я змінної: ")
+
+valid = True
+
+if not name:
+    valid = False
+
 if name[0].isdigit():
-    print(" Ім’я не може починатися з цифри.")
-elif any(char.isupper() for char in name):
-    print("Ім’я не може містити великі літери.")
-elif any(char in string.punctuation.replace("_", "") or char == " " for char in name):
-    print(" Ім’я не може містити пробіли або знаки пунктуації.")
-elif name.count("_") > 1:
-    print( " не може містити більше одного підкреслення.")
-elif name in keyword.kwlist:
-    print(" Ім’я є зареєстрованим словом у Python.")
-else:
-    print(" Це валідне ім’я змінної!")
+    valid = False
+
+for char in name:
+    if char.isupper():
+        valid = False
+
+for char in name:
+    if char in string.punctuation and char != "_":
+        valid = False
+
+for char in name:
+    if char.isspace():
+        valid = False
+
+if name in keyword.kwlist:
+    valid = False
+
+if not valid:
+    if name.count("_") > 1:
+        valid = False
+    else:
+        valid = True
+
+print(valid)
